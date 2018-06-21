9003ed25f528f88892a9cad007ff922eca9c5ada
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-21 17:39:04 +0000

    automatically backed up by learn

--- arraysandhashes.rb
+++ arraysandhashes.rb
@@ -1,9 +1,9 @@
 ingredients= [
-  ["rye","sourdough","baguette"]
-  ["ham","salami","turkey"]
-  ["swiss", "munster","cheddar"]
-  ["mayo", "mustard,tabasco"]
-  ]
+  ["rye","sourdough","baguette"],
+  ["ham","salami","turkey"],
+  ["swiss", "munster","cheddar"],
+  ["mayo", "mustard,tabasco"],
+  ],
   
   puts ingredients[0][1]
   puts ingredients[1]
